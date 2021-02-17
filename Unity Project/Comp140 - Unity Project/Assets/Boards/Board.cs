4fac1dfcd715345d96148435cc9b33ee4b0ca1bc
Andrew <as243879@falmouth.ac.uk>
2021-02-17 11:05:57 +0000

    Created basic board and coordinate system.
    Player can input a number to move their character to the space the number correlates to.

---
+++ Unity Project/Comp140 - Unity Project/Assets/Boards/Board.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width = 3;
+    public int height = 3;
+    private int boardSize;
+
+    public GameObject[] spaces;
+
+    private void Awake()
+    {
+        boardSize = height * width;
+    }
+
+    public Transform GetSpace(int space)
+    {
+        return spaces[space].transform.Find("Anchor");
+    }
+}
---
+++ Unity Project/Comp140 - Unity Project/Assets/Characters/ControllerScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    public Board board;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("0"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(0);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(1);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(2);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(3);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(4);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("5"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(5);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("6"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(6);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("7"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(7);
+
+            this.transform.position = setTransform.position;
+        }
+
+        if (Input.GetKeyDown("8"))
+        {
+            Debug.Log("KeyPressed");
+
+            Transform setTransform = board.GetSpace(8);
+
+            this.transform.position = setTransform.position;
+        }
+    }
+}
