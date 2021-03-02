259b1317fb42225a9af407bf53e0b14cc74f5808
Andrew <as243879@falmouth.ac.uk>
2021-03-02 11:05:27 +0000

    Added multiple enemies.

---
+++ Unity Project/Comp140 - Unity Project/Assets/Characters/Test Enemy/RandomMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public FullBoard board;
+    public Board teamBoard;
+    private int currentSpace;
+
+    private void Start()
+    {
+        RandomMoveSpace();
+    }
+
+    public void RandomMoveSpace()
+    {
+        int space = Random.Range(0, 6);
+
+        Space script = board.spaces[space].GetComponent<Space>();
+
+        if (board.IsSpaceValid(space) && script.GetSpace())
+        {
+            Move(space);
+        }
+        else
+        {
+            RandomMoveSpace();
+        }
+    }
+
+    public void Move(int spaceIndex)
+    {
+        SetSpace(currentSpace, null);
+
+        Transform setTransform = teamBoard.GetSpace(spaceIndex);
+        currentSpace = spaceIndex;
+        this.transform.position = setTransform.position;
+
+        SetSpace(currentSpace, this.gameObject);
+    }
+
+    public void SetSpace(int space, GameObject newCharacter)
+    {
+        Space spaceScript = board.spaces[space].GetComponent<Space>();
+
+        spaceScript.SetSpace(newCharacter);
+    }
+}
